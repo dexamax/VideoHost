b49151045284dafeb4515dcae338cdf58d761503
dexamax <166790203+dexamax@users.noreply.github.com>
2025-05-02 21:55:37 +0530

    Update redirectlink.js

--- videos/redirectlink.js
+++ videos/redirectlink.js
@@ -1,17 +1,1 @@
-(function () {
-  const REDIRECT_URL = "https://whoushex.top/4/9122239";
-  const COOLDOWN_TIME = 2 * 60 * 1000; // 2 minutes in millisecondsa
-  const STORAGE_KEY = "lastRedirectTimestamp";
-
-  const now = Date.now();
-  const lastRedirect = localStorage.getItem(STORAGE_KEY);
-
-  // Check if 2 minutes have passed
-  if (!lastRedirect || now - lastRedirect > COOLDOWN_TIME) {
-    // Set timeout for redirect after 4 seconds
-    setTimeout(function () {
-      localStorage.setItem(STORAGE_KEY, Date.now());
-      window.location.replace(REDIRECT_URL);
-    }, 20000); // 4 seconds
-  }
-})();
+const REDIRECT_URL = "https://whoushex.top/4/9122239";
